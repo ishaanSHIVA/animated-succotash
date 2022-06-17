6d0fcf0e297db78136840a51bff0c46e8f583906
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-18 03:24:56 +0530

    sidebar template

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import styles from "../styles/Home.module.css";
 
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <h1>hi</h1>
+      <Sidebar></Sidebar>
+    </>
+  );
 }
