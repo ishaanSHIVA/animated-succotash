b02a3037b443066e801fb9bdbf5c4b95d42e0f1e
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-18 03:37:32 +0530

    remove context

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import Image from "next/image";
+import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+import { FaBox } from "react-icons/fa";
+import { BsFillBookmarkFill, BsFillPersonFill } from "react-icons/bs";
+import { AiOutlineHistory } from "react-icons/ai";
+import { useRouter } from "next/router";
+
+import logo from "../assets/amazon_logo.png";
+import logoFull from "../assets/amazon_logo_full.png";
+
+import React from "react";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  return <div>Sidebar</div>;
+};
 
-export default Sidebar
+export default Sidebar;
